614861a5bf7742817f827552c06e403f5ee15cd8
Jim McAndrew <jim@loc8.us>
2021-07-02 17:18:06 +0000

    Changes for editor

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "Started"
 chown jekyll:jekyll -R /usr/gem
 touch /srv/jekyll/Gemfile.lock
-chown jekyll:jekyll /srv/jekyll/Gemfile.lock
+chown -R jekyll:jekyll /srv/jekyll/
 bundle update
 bundle install
 JEKYLL_ENV=production bundle exec jekyll build
